0546ea3622b4f967ee9bc5d743398ac91f88e286
William <wnmerukini@gmail.com>
2019-01-10 04:13:18 +0200

    The fuck it was here

--- SeaBotGUI/Form1.cs
+++ SeaBotGUI/Form1.cs
@@ -17,7 +17,6 @@
 using Newtonsoft.Json;
 using SeaBotCore;
 using SeaBotCore.Data;
-using SeaBotCore.Data.Defenitions;
 using SeaBotCore.Data.Materials;
 using SeaBotCore.Logger;
 using SeaBotCore.Utils;
@@ -28,7 +27,6 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
